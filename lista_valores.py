a639540b21ced0fed140ad962ea25d08d950ceef
Almarazz <123679060+Almarazz@users.noreply.github.com>
2023-08-25 20:18:52 -0600

    ejercicio en clase

---
+++ lista_valores.py
@@ -0,0 +1,14 @@
+dias = ['lunes', 'martes', 'miercoles',
+        'jueves', 'viernes', 'sabado', 'domingo']
+
+dias[1] ='MARTES'
+print(dias)
+
+dias[2:4] = ['MIERCOLES', 'JUEVES']
+print(dias)
+
+dias[4:]= ['VIERNES']
+print(dias)
+
+dias[0] = 'LUNES'
+print(dias)

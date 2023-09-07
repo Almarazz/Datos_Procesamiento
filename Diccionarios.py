0b215ca48661c37e0483d6ce25a532371947ac49
Almarazz <123679060+Almarazz@users.noreply.github.com>
2023-09-06 18:20:11 -0600

    avances en clases presenciales

--- Diccionarios.py
+++ Diccionarios.py
@@ -1,6 +1,6 @@
 peliculas ={
     10:'The GodFather',
-    20:'Jurassic park',
+    20:"Jurassic park'}",
     30:'Ex-Machina',
     40:'The Notebook'
 }
@@ -9,7 +9,17 @@ print(len(peliculas))
 print(peliculas.keys())
 print(peliculas.values())
 
-print(peliculas(40))
+
+print(peliculas[40])
 print(peliculas.get(40))
 
 print(peliculas.items())
+
+peliculas[50] = 'Star Wars'
+peliculas.update({60:"Unknown"})
+print(peliculas)
+
+peliculas[60] = 'Casino Royale'
+print(peliculas)
+
+

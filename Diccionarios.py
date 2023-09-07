5ae97895d34ed30592ef37a6b8914daa69a4b3fb
Almarazz <123679060+Almarazz@users.noreply.github.com>
2023-09-06 18:05:34 -0600

    clases

---
+++ Diccionarios.py
@@ -0,0 +1,15 @@
+peliculas ={
+    10:'The GodFather',
+    20:'Jurassic park',
+    30:'Ex-Machina',
+    40:'The Notebook'
+}
+
+print(len(peliculas))
+print(peliculas.keys())
+print(peliculas.values())
+
+print(peliculas(40))
+print(peliculas.get(40))
+
+print(peliculas.items())
